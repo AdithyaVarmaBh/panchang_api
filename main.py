0d03b829becf023ac7a8565a681b255bee3485d5
Durga <durgaadithyavarma.bh@gmail.com>
2026-08-12 23:33:29 -0400

    adding more endpoint

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
 from datetime import datetime
-from astro_core import get_panchang_at_sunrise
+from astro_core import get_panchang_at_sunrise, to_julian_day, get_tithi, get_nakshatra
 
 app = FastAPI()
 
-@app.post("/panchang")
-def panchang_endpoint(payload: dict):
+@app.post("/panchang_sunrise")
+def panchang_sunrise_endpoint(payload: dict):
     dt = datetime.fromisoformat(payload["datetime_iso"])
     tz = payload["timezone"]
     lat = payload["latitude"]
     lon = payload["longitude"]
     return get_panchang_at_sunrise(dt, lat, lon, tz)
+
+@app.post("/panchang_time")
+def panchang_time_endpoint(payload: dict):
+    dt = datetime.fromisoformat(payload["datetime_iso"])
+    tz = payload["timezone"]
+    lat = payload["latitude"]
+    lon = payload["longitude"]
+
+    jd = to_julian_day(dt)
+    tithi = get_tithi(jd)
+    nakshatra = get_nakshatra(jd)
+
+    return {
+        "datetime": dt.isoformat(),
+        "tithi": tithi,
+        "nakshatra": nakshatra
+    }
